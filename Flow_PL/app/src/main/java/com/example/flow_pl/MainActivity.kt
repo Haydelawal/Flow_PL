eca95f06b72624d9ecf0535515e1b9abca55ed69
LAWAL <haydelawal@gmail.com>
2022-11-06 23:22:03 +0100

    shared flow

---
+++ Flow_PL/app/src/main/java/com/example/flow_pl/MainActivity.kt
@@ -0,0 +1,80 @@
+package com.example.flow_pl
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import com.example.flow_pl.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+    //activity_main ==>> Activity_Main_Binding
+    private lateinit var binding: ActivityMainBinding
+    private val myViewModel: MyViewModel by viewModels()
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.textView.text = myViewModel.stateFlow.value.toString()
+
+        binding.button.setOnClickListener {
+            myViewModel.incrementCounter()
+
+
+//            lifecycleScope.launchWhenCreated {
+//                repeatOnLifecycle(Lifecycle.State.STARTED) {
+//                    myViewModel.stateFlow.collectLatest {
+//                        binding.textView.text = it.toString()
+//                    }
+//                }
+//            }
+//
+//        }
+            collectLatestLifeCycleFlow(myViewModel.stateFlow) {
+
+                binding.textView.text = it.toString()
+            }
+
+            collectLifeCycleFlow(myViewModel.sharedFlow){
+                //etc
+            }
+
+
+        }
+
+    }
+}
+
+// for state flow
+fun <T> AppCompatActivity.collectLatestLifeCycleFlow(flow: Flow<T>, collect: suspend (T) -> Unit) {
+    lifecycleScope.launch {
+        repeatOnLifecycle(Lifecycle.State.STARTED) {
+            flow.collectLatest(collect)
+        }
+
+    }
+}
+
+
+// for shared flow
+fun <T> AppCompatActivity.collectLifeCycleFlow(flow: Flow<T>, collect: suspend (T) -> Unit) {
+    lifecycleScope.launch {
+        repeatOnLifecycle(Lifecycle.State.STARTED) {
+            flow.collect{collect}
+        }
+
+    }
+}
+
+
